3d2c1a6c5e04aa5a131bab468e6d1fd33353260c
Tanapol Rattanapichetkul <tanapol.raa@gmail.com>
2025-01-26 01:41:42 +0700

    add stage boundary

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -4,10 +4,16 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
     public Vector3 offset;
+    public Vector3 minValue;
+    public Vector3 maxValue;
     
     void Update()
     {
-        Vector3 playerPos = player.transform.position;;
-        gameObject.transform.position = new Vector3 (playerPos.x + offset.x, playerPos.y + offset.y, offset.z); // Camera follows the player with specified offset position
+        Vector3 playerPos = player.transform.position;
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(playerPos.x, minValue.x, maxValue.x),
+            Mathf.Clamp(playerPos.y, minValue.y, maxValue.y),
+            Mathf.Clamp(playerPos.z, minValue.z, maxValue.z));
+        gameObject.transform.position = new Vector3 (boundPosition.x + offset.x, boundPosition.y + offset.y, offset.z); // Camera follows the player with specified offset position
     }
 }
