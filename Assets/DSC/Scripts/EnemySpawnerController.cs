8c2e4fbae78202410c4d437c2e0fe636b0a4541e
DeStiCap <53054030+DeStiCap@users.noreply.github.com>
2025-01-25 21:34:12 +0700

    improve enemy spawner for group spawn

--- Assets/DSC/Scripts/EnemySpawnerController.cs
+++ Assets/DSC/Scripts/EnemySpawnerController.cs
@@ -7,10 +7,21 @@ namespace GGJ2025
         #region Variable
 
         [SerializeField] EnemyController[] m_EnemyPrefabs;
+
+        [Min(1)]
+        [SerializeField] int m_SpawnCountPerRound = 1;
+        [SerializeField] bool m_InitSpawn = true;
+        [Min(0f)]
+        [SerializeField] float m_RandomAreaSizeX = 1f;
+        [Min(0f)]
+        [SerializeField] float m_RandomAreaSizeY = 1f;
+
+        [Header("Loop Spawn")]
         [Min(0.01f)]
-        [SerializeField] float m_SpawnDelayMin = 1f;
+        [SerializeField] float m_LoopDelayMin = 1f;
         [Min(0.01f)]
-        [SerializeField] float m_SpawnDelayMax = 2f;
+        [SerializeField] float m_LoopDelayMax = 2f;
+        [SerializeField] bool m_LoopSpawn;
 
         float m_NextSpawnTime;
 
@@ -18,16 +29,55 @@ namespace GGJ2025
 
         #region Main
 
+        private void Awake()
+        {
+            if (m_InitSpawn)
+            {
+                for(int i = 0; i < m_SpawnCountPerRound; i++)
+                {
+                    RandomSpawnEnemyInList();
+                }
+            }
+        }
+
         private void Update()
         {
-            if(Time.time >= m_NextSpawnTime)
+            if (m_LoopSpawn)
             {
-                m_NextSpawnTime = Time.time + Random.Range(m_SpawnDelayMin, m_SpawnDelayMax);
+                LoopingSpawn();
+            }            
+        }
 
-                var randomID = Random.Range(0, m_EnemyPrefabs.Length);
+        void LoopingSpawn()
+        {
+            if (Time.time >= m_NextSpawnTime)
+            {
+                m_NextSpawnTime = Time.time + Random.Range(m_LoopDelayMin, m_LoopDelayMax);
 
-                Instantiate(m_EnemyPrefabs[randomID], transform.position, Quaternion.identity);
+                for (int i = 0; i < m_SpawnCountPerRound; i++)
+                {
+                    RandomSpawnEnemyInList();
+                }
             }
+        }
+
+        void RandomSpawnEnemyInList()
+        {
+            var randomID = Random.Range(0, m_EnemyPrefabs.Length);
+
+            var prefab= m_EnemyPrefabs[randomID];
+            Instantiate(prefab, GetRandomSpawnLocation(), prefab.transform.rotation);
+        }
+
+        Vector3 GetRandomSpawnLocation()
+        {
+            var pos = transform.position;
+
+            pos.x += Random.Range(-m_RandomAreaSizeX, m_RandomAreaSizeX);
+            pos.y += Random.Range(-m_RandomAreaSizeY, m_RandomAreaSizeY);
+
+
+            return pos;
         }
 
         #endregion
